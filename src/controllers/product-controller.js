d8f6261881151b9731d90106203a1ef0906b773b
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2023-08-03 13:25:56 -0300

    Listando um produto pelo slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,7 +2,18 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 exports.get = (req, res, next) => {
-  Product.find({}, 'title price slug')
+  Product.find({active: true}, 'title price slug')
+    .then((e) => {
+      res.status(201).send(e);
+    })
+    .catch((e) => {
+      res
+        .status(400)
+        .send({message: 'Falha ao cadastrar produto', data: e.message});
+    });
+};
+exports.getBySlug = (req, res, next) => {
+  Product.findOne({slug: req.params.slug, active: true}, 'title price slug')
     .then((e) => {
       res.status(201).send(e);
     })
